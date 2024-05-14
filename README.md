2264e818a93ee1089a4db996506d6fabba76d096
Olivier Dusabamahoro <161004706+Olivier2201@users.noreply.github.com>
2024-05-14 11:50:58 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+Welcome to my GitHub portfolio! This collection showcases my journey in software development and coding. 
+From projects that demonstrate my skills in various languages and frameworks to contributions in open-source communities, 
+each repository here reflects my passion for writing clean, efficient code and solving complex problems. Check out my commits, 
+pull requests, and discussions to see how I have been continuously learning and improving. I'm excited to share my development journey with you!
---
+++ behavior.js
@@ -0,0 +1,33 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzvhT0-ORXVmGShudr7elQE-FeHLxy4bGsbUJUyRSXOHnFwH9N90jBdQ82-i8iwI_A/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
